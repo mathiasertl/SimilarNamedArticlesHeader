6f2ca1a70959a2aa876752b81a7880187284c577
Mathias Ertl <mati@fsinf.at>
2013-05-26 15:24:02 +0200

    minor modernization

--- SimilarNamedArticlesHeader.php
+++ SimilarNamedArticlesHeader.php
@@ -1,75 +1,76 @@
 <?php
 
-$wgHooks['OutputPageBeforeHTML'][] = 'SimilarNamedArticlesHeader';
+$wgHooks['OutputPageBeforeHTML'][] = 'fnSimilarNamedArticlesHeader';
 
 $wgExtensionCredits['other'][] = array (
-	'name' => 'SimilarNamedArticlesHeader',
-	'description' => 'Displays articles with a similar name on top of each page',
-	'version' => '2.0.1',
-	'author' => 'Mathias Ertl',
-	'url' => 'http://fs.fsinf.at/wiki/SimilarNamedArticlesHeader',
+    'path' => __file__
+    'name' => 'SimilarNamedArticlesHeader',
+    'description' => 'Displays articles with a similar name on top of each page',
+    'version' => '2.0.1',
+    'author' => 'Mathias Ertl',
+    'url' => 'http://fs.fsinf.at/wiki/SimilarNamedArticlesHeader',
 );
 
-function SimilarNamedArticlesHeader( $output_page, $qText)
+function fnSimilarNamedArticlesHeader($output_page, $qText)
 {
-	global $wgTitle, $wgRequest;
-	global $wgSimilarNamedArticlesHeaderEnable;
-	if (!$wgSimilarNamedArticlesHeaderEnable) {
-		return true;
-	}
-	
-	global $wgSimilarNamedArticlesHeaderOnNamespaces;
-	global $wgSimilarNamedArticlesHeaderOnSubpages;
+    global $wgTitle, $wgRequest;
+    global $wgSimilarNamedArticlesHeaderEnable;
+    if (!$wgSimilarNamedArticlesHeaderEnable) {
+        return true;
+    }
 
-	if ( is_array( $wgSimilarNamedArticlesHeaderOnNamespaces ) ) {
-		$tmpArray = array_keys( $wgSimilarNamedArticlesHeaderOnNamespaces, true);
-		if (!in_array($wgTitle->getNamespace(), $tmpArray)) {
-			return true;
-		}
-	}
-	if ( ! $wgSimilarNamedArticlesHeaderOnSubpages && $wgTitle->isSubpage() ) {
-		$baseTitle = Title::newFromText(
-			$wgTitle->getBaseText(),
-			$wgTitle->getNamespace() );
-		if ( $baseTitle->exists() ) {
-			return true;
-		}
-	}
+    global $wgSimilarNamedArticlesHeaderOnNamespaces;
+    global $wgSimilarNamedArticlesHeaderOnSubpages;
 
-	# dont show when we edit pages:
-	if ( $wgRequest->getVal('action') != '' )
-		return true;
+    if (is_array($wgSimilarNamedArticlesHeaderOnNamespaces)) {
+        $tmpArray = array_keys($wgSimilarNamedArticlesHeaderOnNamespaces, true);
+        if (!in_array($wgTitle->getNamespace(), $tmpArray)) {
+            return true;
+        }
+    }
+    if (! $wgSimilarNamedArticlesHeaderOnSubpages && $wgTitle->isSubpage()) {
+        $baseTitle = Title::newFromText(
+            $wgTitle->getBaseText(),
+            $wgTitle->getNamespace());
+        if ($baseTitle->exists()) {
+            return true;
+        }
+    }
 
-	global $wgParser;
-	global $wgSimilarNamedArticlesHeaderIncludeSubpages;
-	global $wgSimilarNamedArticlesHeaderPREG;
+    # dont show when we edit pages:
+    if ($wgRequest->getVal('action') != '')
+        return true;
 
-	$snaPage = new SimilarNamedArticles();
+    global $wgParser;
+    global $wgSimilarNamedArticlesHeaderIncludeSubpages;
+    global $wgSimilarNamedArticlesHeaderPREG;
 
-	$titleText = $wgTitle->getText();
+    $snaPage = new SimilarNamedArticles();
 
-	# This is custimized for our needs. The List of SimilarNamedArticles
-	# that is displayed above each article should be created from a
-	# searchstring that is NOT the entire title (it would always only find
-	# itself) but rather only the first part (in our case only 'til the
-	# regular expression).
-	$searchstring = preg_replace ( $wgSimilarNamedArticlesHeaderPREG, '', $titleText);
+    $titleText = $wgTitle->getText();
 
-	$title = Title::newFromtext( $searchstring );
+    # This is custimized for our needs. The List of SimilarNamedArticles
+    # that is displayed above each article should be created from a
+    # searchstring that is NOT the entire title (it would always only find
+    # itself) but rather only the first part (in our case only 'til the
+    # regular expression).
+    $searchstring = preg_replace ($wgSimilarNamedArticlesHeaderPREG, '', $titleText);
 
-	# this gets the text that is actually prepended.
-	$output = $snaPage->getSimilarNames( $title, NULL, false );
+    $title = Title::newFromtext($searchstring);
 
-	if ( $output != '' ) {
-		$parserOutput = $wgParser->parse( "$output\n", $wgTitle, $output_page->parserOptions(),
-			true, true, $output_page->mRevisionId );
-		$output_page->addParserOutputNoText( $parserOutput );
+    # this gets the text that is actually prepended.
+    $output = $snaPage->getSimilarNames($title, NULL, false);
 
-		# you could invert this to append the text instead of prepending it.
-		$qText =  $parserOutput->getText() . $qText;
-	}
+    if ($output != '') {
+        $parserOutput = $wgParser->parse("$output\n", $wgTitle, $output_page->parserOptions(),
+            true, true, $output_page->mRevisionId);
+        $output_page->addParserOutputNoText($parserOutput);
 
-	return true;
+        # you could invert this to append the text instead of prepending it.
+        $qText =  $parserOutput->getText() . $qText;
+    }
+
+    return true;
 }
 
 ?>
